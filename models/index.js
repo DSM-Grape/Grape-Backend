2825c66286b730bc91a64b7e451582edc3312f6c
JoMingyu <city7310@naver.com>
2018-05-30 14:57:46 +0900

    [UPDATE] models/index.js - Lint 적용

--- models/index.js
+++ models/index.js
@@ -1,48 +1,49 @@
-'use strict';
+
 
 const Sequelize = require('sequelize');
+
 const sequelize = new Sequelize(
-    'grape',
-    'root',
-    process.env.GRAPE_MYSQL_PW,
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
+  'grape',
+  'root',
+  process.env.GRAPE_MYSQL_PW,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+  },
 );
 
 const accounts = sequelize.define('tbl_accounts', {
-    uuid: {
-        type: Sequelize.CHAR(32),
-        primaryKey: true
-    },
-    id: {
-        type: Sequelize.STRING(127),
-        unique: true
-    },
-    password: {
-        type: Sequelize.STRING(200),
-        allowNull: true,
-        defaultValue: null
-    },
-    plan: {
-        type: Sequelize.ENUM('FREE', 'BUSINESS', 'FIRST'),
-        defaultValue: 'FREE'
-    },
-    email: {
-        type: Sequelize.STRING(63),
-        allowNull: true,
-        defaultValue: null
-    },
-    nickname: {
-        type: Sequelize.STRING(63),
-        allowNull: true,
-        defaultValue: null
-    }
+  uuid: {
+    type: Sequelize.CHAR(32),
+    primaryKey: true,
+  },
+  id: {
+    type: Sequelize.STRING(127),
+    unique: true,
+  },
+  password: {
+    type: Sequelize.STRING(200),
+    allowNull: true,
+    defaultValue: null,
+  },
+  plan: {
+    type: Sequelize.ENUM('FREE', 'BUSINESS', 'FIRST'),
+    defaultValue: 'FREE',
+  },
+  email: {
+    type: Sequelize.STRING(63),
+    allowNull: true,
+    defaultValue: null,
+  },
+  nickname: {
+    type: Sequelize.STRING(63),
+    allowNull: true,
+    defaultValue: null,
+  },
 }, {
-    timestamps: false
+  timestamps: false,
 });
 
-sequelize.sync()
+sequelize.sync();
 
 module.exports = sequelize;
