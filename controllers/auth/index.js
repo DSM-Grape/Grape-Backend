f2629b4a891026ad9c8115922ded74e6db53a687
JoMingyu <city7310@naver.com>
2018-06-12 08:38:45 +0900

    [DONE] GET /email/certify/:email API

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const {
   hello,
   emailDuplicationCheck,
+  sendEmail,
 } = require('./auth.controller');
 
 router.route('/').get(hello);
 router.route('/check/email/:email').get(emailDuplicationCheck);
+router.route('/email/certify/:email').get(sendEmail);
 
 module.exports = router;
