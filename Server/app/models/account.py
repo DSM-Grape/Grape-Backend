4e94c4c87c19239e8bf6a4a8171ec9b22f190054
JoMingyu <city7310@naver.com>
2018-06-17 21:51:09 +0900

    [DONE] TokenModel, Access/RefreshTokenModel

--- Server/app/models/account.py
+++ Server/app/models/account.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from mongoengine import *
 
 
@@ -26,3 +28,44 @@ class AccountModel(Document):
     nickname = StringField()
     # required=False
 
+
+class TokenModel(Document):
+    meta = {
+        'abstract': True,
+        'allow_inheritance': True
+    }
+
+    class Key(EmbeddedDocument):
+        owner = ReferenceField(
+            document_type=AccountModel,
+            required=True
+        )
+
+        user_agent = StringField(
+            required=True
+        )
+
+    key = EmbeddedDocumentField(
+        document_type=Key,
+        primary_key=True
+    )
+    # 여러 필드를 합쳐 PK로 두기 위함
+
+    identity = UUIDField(
+        unique=True,
+        default=uuid4
+    )
+
+
+class AccessTokenModel(Document):
+    meta = {
+        'collection': 'access_token'
+    }
+
+
+class RefreshTokenModel(Document):
+    meta = {
+        'collection': 'refresh_token'
+    }
+
+    pw_snapshot = StringField()
