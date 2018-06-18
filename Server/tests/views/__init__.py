dc7d5b4d15ea3ce50acb8aa4043c140fdef375c8
JoMingyu <city7310@naver.com>
2018-06-18 12:21:21 +0900

    [DONE] TCBase
    - primary_user 정의
    - fb_user 정의
    - 두 사용자의 token 정의

--- Server/tests/views/__init__.py
+++ Server/tests/views/__init__.py
@@ -6,7 +6,11 @@ from unittest import TestCase as TC
 import pymongo
 from flask import Response
 
+from werkzeug.security import generate_password_hash
+
 from app import create_app
+from app.models.account import AccountModel, AccessTokenModel, RefreshTokenModel
+
 from config.test import TestConfig
 
 
@@ -26,15 +30,27 @@ class TCBase(TC):
         super(TCBase, self).__init__(*args, **kwargs)
 
     def _create_fake_account(self):
-        self.primary_user = None
-        self.secondary_user = None
+        self.primary_user_pw = 'primary'
+
+        self.primary_user = AccountModel(
+            id='primary',
+            pw=generate_password_hash(self.primary_user_pw),
+            email_certified=True,
+            nickname='primary'
+        ).save()
+
+        self.fb_user = AccountModel(
+            id='secondary',
+            email_certified=False,
+            nickname='secondary'
+        ).save()
 
     def _generate_tokens(self):
         with self.app.app_context():
-            self.primary_user_access_token = None
-            self.primary_user_refresh_token = None
-            self.secondary_user_access_token = None
-            self.secondary_user_refresh_token = None
+            self.primary_user_access_token = AccessTokenModel.create_access_token(self.primary_user, 'TEST')
+            self.primary_user_refresh_token = RefreshTokenModel.create_refresh_token(self.primary_user, 'TEST')
+            self.fb_user_access_token = AccessTokenModel.create_access_token(self.fb_user, 'TEST')
+            self.fb_user_refresh_token = RefreshTokenModel.create_refresh_token(self.fb_user, 'TEST')
 
     def setUp(self):
         self._create_fake_account()
